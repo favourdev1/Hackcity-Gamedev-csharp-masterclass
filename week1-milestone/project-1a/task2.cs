e351198d2f88f3a7dbc8197d9b1d9c81ea78cfac
Favourpro <favourapps17@gmail.com>
2022-10-28 14:12:43 +0100

    added task 2 file

---
+++ week1-milestone/project-1a/task2.cs
@@ -0,0 +1,25 @@
+using System;
+
+ namespace Helloworld
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       
+       //QUESTION
+       //Declare five variables choosing for each of them the most appropriate of the types byte, 
+       //sbyte, short, ushort, int, uint, long, ulong to represent the following 
+       //values: 52130, -115, 4825932, 97, -10000
+       
+       sbyte  num1 = -115;
+       sbyte  num2 =97;
+       short num3 = -10000;
+       int num4 = 52130;
+       int num5 = 4825932; 
+       
+     } 
+     
+   }
+ }
+ 
