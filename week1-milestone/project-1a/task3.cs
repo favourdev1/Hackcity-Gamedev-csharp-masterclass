7bcc315a0793e190ab2b5fb7344016218d5f3e2c
Favourpro <favourapps17@gmail.com>
2022-10-28 14:33:02 +0100

    added task3 file

---
+++ week1-milestone/project-1a/task3.cs
@@ -0,0 +1,27 @@
+using System;
+
+ namespace Helloworld
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       //QUESTION
+       //A marketing firm wants to keep a record of its employees. 
+       //Each record would have the following 
+       //characteristics – first name, family name, age, gender (m or f), ID number, unique employee number (27560000 to 27569999). 
+       //Declare the variables needed to keep the information for a single employee using appropriate data types and descriptive names.
+       
+       string firstName = "Favour";
+       string familyName = "Okechukwu";
+       sbyte age = 29;
+       char gender ="m";
+       short idNum = 206;
+       int employeeNumber = 27560001;
+        
+       
+     } 
+     
+   }
+ }
+
