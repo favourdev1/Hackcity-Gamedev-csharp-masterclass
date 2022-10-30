43660832e68b92f2de845fcca405a2469f617d85
Favour Okechukwu <favourapps17@gmail.com>
2022-10-30 14:34:35 +0100

    Create test1.cs

---
+++ week1-milestone/Conditional_Statements_and_Loops/test1.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+// Compiler version 4.0, .NET Framework 4.5
+
+
+namespace Assignment
+{
+    public class Program
+    {
+        public static void Main(string[] args) {
+            Console.Write(takeVal());
+
+
+            Console.Read();
+        }
+
+
+
+
+
+        public static string takeVal() {
+            Console.Write("Please enter a value:");
+            string value = Console.ReadLine();
+            double num;
+
+
+            try {
+                num = Double.Parse(value);
+            }catch(Exception) {
+                return "please ensure to only enter a number";
+            }
+
+
+            string[] unit = {
+                "Zero",
+                "One",
+                "Two",
+                "Three",
+                "Four",
+                "Five",
+                "Six",
+                "Seven",
+                "Eight",
+                "Nine",
+                "Ten",
+                "Eleven",
+                "Twelve",
+                "Thirteen",
+                "Fourteen",
+                "Fifteen",
+                "Sixteen",
+                "Seventeen",
+                "Eighteen",
+                "Nineteen"
+            };
+
+
+
+
+            switch (num < 20) {
+                case (false):
+                    return "please enter a value between 0 and 19";
+
+                    //
+                    //  takeVal();
+                    default:
+                        return unit [(int)num];
+                    }
+                    return "";
+            }
+
+        }
+    }
