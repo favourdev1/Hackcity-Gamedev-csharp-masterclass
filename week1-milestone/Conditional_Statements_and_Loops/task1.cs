e14f3fa4f07b74f40056637f1f2e038930921579
Favour Okechukwu <favourapps17@gmail.com>
2022-10-30 14:35:50 +0100

    Update and rename test1.cs to task1.cs

--- week1-milestone/Conditional_Statements_and_Loops/task1.cs
+++ week1-milestone/Conditional_Statements_and_Loops/task1.cs
@@ -10,7 +10,11 @@ namespace Assignment
     public class Program
     {
         public static void Main(string[] args) {
-            Console.Write(takeVal());
+            
+//Write a program that asks for a digit and depending on the input shows the name of that digit (in English) using a switch statement.
+
+
+Console.Write(takeVal());
 
 
             Console.Read();
